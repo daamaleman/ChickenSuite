63f6b987d7ef5e44c043e47bd467c503dff8fd18
daamaleman <diedereicha@uamv.edu.ni>
2025-11-13 18:32:28 -0600

    Agrego plantilla con conexion a postgresql del proyecto

---
+++ src/main/java/com/tuempresa/chickenSuiteApp/run/chickenSuiteApp.java
@@ -0,0 +1,18 @@
+package com.tuempresa.chickenSuiteApp.run;
+
+import org.openxava.util.*;
+
+/**
+ * Ejecuta esta clase para arrancar la aplicación.
+ *
+ * Con OpenXava Studio/Eclipse: Botón derecho del ratón > Run As > Java Application
+ */
+
+public class chickenSuiteApp {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("chickenSuiteApp-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		AppServer.run("chickenSuiteApp"); // Usa AppServer.run("") para funcionar en el contexto raíz
+	}
+
+}
